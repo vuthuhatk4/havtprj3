b2e331b38a6878047f846c004c7f4f5f82bcb36d
vuthuhatk4 <vuthuhatk4@gmail.com>
2023-09-17 23:25:06 +0700

    success_full

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a722f00b5203a4a0e8978601599986cd-1317198628.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
